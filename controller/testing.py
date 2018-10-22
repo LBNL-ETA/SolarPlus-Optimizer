8c2b6ea3e231f10a59bd0bd18e6e4c1690aef7a4
UbuntuWS <dhblum@lbl.gov>
2018-10-22 09:50:43 -0700

    Use configuration file.

--- controller/testing.py
+++ controller/testing.py
@@ -3,92 +3,26 @@
 This module contains unit tests of the MPC controller.
 
 """
-from mpcpy import units
 import unittest
 from mpc import mpc
-import os
 from matplotlib import pyplot as plt
 import numpy as np
+import mpc_config
 
-model_config =      {'mopath' : os.path.join('models','SolarPlus.mo'),
-                     'modelpath' : 'SolarPlus.Building.Optimization.Store',
-                     'libraries' : os.getenv('MODELICAPATH'),
-                     'measurements' : ['Trtu', 'Tref', 'Tfre'],
-                     'other_outputs' : ['Pnet', 'Prtu', 'Pref', 'Pfre','Pcharge', 'Pdischarge', 'SOC'],
-                     'sample_rate' : 3600,
-                     'parameters' : {'Name':      ['Trtu_0', 'Tref_0', 'Tfre_0', 'SOC_0'], 
-                                     'Free':      [False,    False,    False,    False],
-                                     'Value':     [0,        0,        0,        0.25],
-                                     'Minimum':   [10,       0,        -40,      0],
-                                     'Maximum':   [35,       20,       0,        1],
-                                     'Covariance':[0,        0,        0,        0],
-                                     'Unit' :     ['degC',   'degC',   'degC',   '1']},
-                      'init_vm' : {'Trtu_0' : 'Trtu',
-                                   'Tref_0' : 'Tref',
-                                   'Tfre_0' : 'Tfre'}}
-
-opt_config =        {'problem'  : 'EnergyCostMin',
-                     'power_var': 'J'} 
-
-weather_config =    {'type': 'csv',
-                     'path': os.path.join('data','Temperature.csv'),
-                     'vm'  : {'Outdoor':('weaTDryBul', units.degC),
-                              'Solar Radiation':('weaHGloHor', units.W_m2)},
-                     'geo' : (40.88,-124.0)}
-                               
-control_config =    {'type': 'csv',
-                     'path': os.path.join('data','Control2.csv'),
-                     'vm'  : {'HVAC1_Norm' : ('uCool', units.unit1),
-                              'RefComp_Norm' : ('uRef', units.unit1),
-                              'FreComp_Split_Norm' : ('uFreCool', units.unit1),
-                              'uHeat' : ('uHeat', units.unit1),
-                              'uCharge' : ('uCharge', units.unit1),
-                              'uDischarge' : ('uDischarge', units.unit1)}}
-                               
-constraint_config = {'type': 'csv',
-                     'path': os.path.join('data','Constraint.csv'),
-                     'vm'  : {'Trtu_min':('Trtu', 'GTE', units.degC),
-                              'Trtu_max':('Trtu', 'LTE', units.degC),
-                              'Tref_min':('Tref', 'GTE', units.degC),
-                              'Tref_max':('Tref', 'LTE', units.degC),
-                              'Tfre_min':('Tfre', 'GTE', units.degC),
-                              'Tfre_max':('Tfre', 'LTE', units.degC),
-                              'SOC_min':('SOC', 'GTE', units.unit1),
-                              'SOC_max':('SOC', 'LTE', units.unit1),
-                              'uCool_min':('uCool', 'GTE', units.unit1),
-                              'uCool_max':('uCool', 'LTE', units.unit1),
-                              'uHeat_min':('uHeat', 'GTE', units.unit1),
-                              'uHeat_max':('uHeat', 'LTE', units.unit1),
-                              'uCharge_min':('uCharge', 'GTE', units.unit1),
-                              'uCharge_max':('uCharge', 'LTE', units.unit1),
-                              'uDischarge_min':('uDischarge', 'GTE', units.unit1),
-                              'uDischarge_max':('uDischarge', 'LTE', units.unit1),
-                              'uRef_min':('uRef', 'GTE', units.unit1),
-                              'uRef_max':('uRef', 'LTE', units.unit1),
-                              'uFreCool_min':('uFreCool', 'GTE', units.unit1),
-                              'uFreCool_max':('uFreCool', 'LTE', units.unit1),
-                              'demand':('Pnet', 'LTE', units.kW)}}
-                              
-price_config =      {'type': 'csv',
-                     'path': os.path.join('data','Price.csv'),
-                     'vm'  : {'pi_e':('pi_e', units.dol_kWh)}}
-
-system_config =     {'type': 'csv',
-                     'path': os.path.join('data','Temperature.csv'),
-                     'vm'  : {'Refrigerator East':('Tref', units.degC),
-                              'HVAC East':('Trtu', units.degC),
-                              'Freezer':('Tfre', units.degC)}}
+config = mpc_config.get_config()
                              
 class unit(unittest.TestCase):
     
     def setUp(self):
         self.start_time = '6/1/2018'
         self.final_time = '6/6/2018'
-        self.controller = mpc(model_config, opt_config, system_config,
-                              weather_config = weather_config,
-                              control_config = control_config,
-                              constraint_config = constraint_config,
-                              price_config = price_config)
+        self.controller = mpc(config['model_config'],
+                              config['opt_config'], 
+                              config['system_config'],
+                              weather_config = config['weather_config'],
+                              control_config = config['control_config'],
+                              constraint_config = config['constraint_config'],
+                              price_config = config['price_config'])
     
     def test_update_exo(self):
         for exo in [self.controller.weather, 
@@ -113,12 +47,14 @@ class functional(unittest.TestCase):
     
     def test_simulate(self):
         # Instantiate
-        model_config['modelpath'] = 'SolarPlus.Building.Optimization.StoreSim'
-        self.controller = mpc(model_config, opt_config, system_config,
-                              weather_config = weather_config,
-                              control_config = control_config,
-                              constraint_config = constraint_config,
-                              price_config = price_config)
+        config['model_config']['modelpath'] = 'SolarPlus.Building.Optimization.StoreSim'
+        self.controller = mpc(config['model_config'],
+                              config['opt_config'], 
+                              config['system_config'],
+                              weather_config = config['weather_config'],
+                              control_config = config['control_config'],
+                              constraint_config = config['constraint_config'],
+                              price_config = config['price_config'])
         # Simulate
         measurements, other_outputs = self.controller.simulate('6/1/2018', '6/6/2018')
         # Plot
@@ -133,11 +69,13 @@ class functional(unittest.TestCase):
         
     def test_optimize(self):
         # Instantiate
-        self.controller = mpc(model_config, opt_config, system_config,
-                              weather_config = weather_config,
-                              control_config = control_config,
-                              constraint_config = constraint_config,
-                              price_config = price_config)
+        self.controller = mpc(config['model_config'],
+                              config['opt_config'], 
+                              config['system_config'],
+                              weather_config = config['weather_config'],
+                              control_config = config['control_config'],
+                              constraint_config = config['constraint_config'],
+                              price_config = config['price_config'])
         # Optimize
         control, measurements, other_outputs, statistics = self.controller.optimize('6/1/2018', '6/2/2018', init=True)
         
