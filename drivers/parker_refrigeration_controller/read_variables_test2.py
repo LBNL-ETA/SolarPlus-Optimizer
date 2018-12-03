73f32c89edf3503f5a7d452d3ef85e609697aa76
Anand Krishnan <anandkp92@gmail.com>
2018-12-03 10:29:07 -0800

    Update read_variables_test2.py

--- drivers/parker_refrigeration_controller/read_variables_test2.py
+++ drivers/parker_refrigeration_controller/read_variables_test2.py
@@ -9,7 +9,7 @@ from pymodbus.payload import BinaryPayloadDecoder
 obj = Modbus_Driver("fridge_config.yaml")
 obj.initialize_modbus()
 
-print("Cabinet probe register reading:")
-print(obj.get_single_register_data(register_name='cab_probe'))
+print("Evaporator probe register reading:")
+print(obj.get_single_register_data(register_name='evap_probe'))
 
 obj.kill_modbus()
