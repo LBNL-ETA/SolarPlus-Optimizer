56993102aa2dc4c7c67a8bab3ae4cc64af5b4290
Anand Krishnan Prakash <anandkp92@gmail.com>
2019-08-21 10:17:40 -0700

    add a process monitoring shell script

---
+++ drivers/xbos_drivers/flexstat/shell.sh
@@ -0,0 +1,25 @@
+#cd /home/solarplus/test_drivers && python run_always.py test_config.yaml
+cd /home/solarplus/xboswave/python/pyxbos/pyxbos/drivers/flexstat
+python flexstat.py flexstat_config.yaml & 
+p0=$!
+echo "$p0" > /home/solarplus/xboswave/python/pyxbos/pyxbos/drivers/flexstat/pid.txt
+now=`date`
+echo "$now: started as $p0"
+
+while true; do
+	status=true
+	while $status; do
+		if [ ! -d /proc/$p0  ]
+		then
+			status=false
+			now=`date`
+			echo "$now: process no longer running"
+		fi
+	done
+	python run_always.py test_config.yaml & 
+	p0=$!
+	echo "$p0" > /home/solarplus/test_drivers/pid.txt
+	now=`date`
+	echo "$now: restarted as $p0"
+done
+
