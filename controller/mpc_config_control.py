c95aaf3de4645234830bfda37c389c2d3b1b9d6a
kuzha <kunzhang@lbl.gov>
2019-12-04 17:20:43 -0800

    change the reference to control both heating and cooling setpoints

--- controller/mpc_config_control.py
+++ controller/mpc_config_control.py
@@ -88,14 +88,6 @@ config={"model_config" :{'mopath' : os.path.join('models','SolarPlus.mo'),
         "influxdb": {"config_filename":"database_client/config.yaml",
                      "section": "database"}
     },
-    # "weather": {
-    #     "type": "csv",
-    #     "variables": {
-    #         "Outdoor": {"filename": "Shadow/Weather_Forecast.csv", "column": "Outdoor", "tz":"America/Los_Angeles", "agg": "mean", "window": "5m"},
-    #         "poa_pv": {"filename": "Shadow/Weather_Forecast.csv", "column": "poa_pv", "tz":"America/Los_Angeles", "agg": "mean", "window": "5m"},
-    #         "poa_win": {"filename": "Shadow/Weather_Forecast.csv", "column": "poa_win", "tz":"America/Los_Angeles", "agg": "mean", "window": "5m"}
-    #     }
-    # },
     "weather": {
         "type": "influxdb",
         "variables": {
@@ -151,8 +143,8 @@ config={"model_config" :{'mopath' : os.path.join('models','SolarPlus.mo'),
         "type": "influxdb",
         "variables": {
             "Tref": {"uuid": "5c69b4b6-22a0-561b-801b-72aee17c5a94", "window": "5m", "agg": "mean", "measurement": "timeseries"},
-            "Trtu": {"uuid": "fd200d7e-0c46-53fc-87e4-6c8639b67b94", "window": "5m", "agg": "mean", "measurement": "timeseries"},
-            #"Trtu": {"uuid": "7d48d689-5cf8-50fd-98af-22dd9868b379", "window": "5m", "agg": "mean", "measurement": "timeseries"},
+            #"Trtu_east": {"uuid": "fd200d7e-0c46-53fc-87e4-6c8639b67b94", "window": "5m", "agg": "mean", "measurement": "timeseries"},
+            "Trtu": {"uuid": "7d48d689-5cf8-50fd-98af-22dd9868b379", "window": "5m", "agg": "mean", "measurement": "timeseries"},
             "Tfre": {"uuid": "3f493b8d-0107-569f-8968-433f46de0fec", "window": "5m", "agg": "mean", "measurement": "timeseries"}
 #            "SOC": {"uuid": "86f72439-35a3-4997-a14f-24f8a889b164", "window": "5m", "agg": "mean", "measurement": "timeseries"}
         }
