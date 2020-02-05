40103a3691e49bcaaa9f20f11567983b757b5c74
Anand Krishnan Prakash <anandkp92@gmail.com>
2020-02-05 14:47:44 -0800

    updating the control config template

--- controller/control.py
+++ controller/control.py
@@ -11,8 +11,8 @@ import os
 import datetime
 import time
 import pandas as pd
-import mpc_shadow_config_template as mpc_config
-from mpc_shadow_config_template import tz_computer
+import mpc_config
+from mpc_config import tz_computer
 from mpc import mpc
 
 def run():
@@ -80,10 +80,11 @@ if __name__ == '__main__':
         time.sleep(1)
         t = datetime.datetime.now()
         print(t)
-        if (t.minute in [0,5,10,15,20,25,30,35,40,45,50,55]) and (t.minute != minute):
-            minute = t.minute
-            try:
-                run()
-                print('Run ended ok.')
-            except:
-                print('Run ended in error.')
+        # if (t.minute in [0,5,10,15,20,25,30,35,40,45,50,55]) and (t.minute != minute):
+        minute = t.minute
+        try:
+            run()
+            print('Run ended ok.')
+            time.sleep(300)
+        except:
+            print('Run ended in error.')
--- controller/mpc_control_config_template.py
+++ controller/mpc_control_config_template.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# rename this to mpc_config.py while doing actual controls
+# rename this to mpc_config.py while running the shadow mode testing
 
 from mpcpy import units
 import os
@@ -9,17 +9,18 @@ tz_computer = 'UTC'
 config={"model_config" :{'mopath' : os.path.join('models','SolarPlus.mo'),
                          'modelpath' : 'SolarPlus.Building.Optimization.Store',
                          'libraries' : os.getenv('MODELICAPATH'),
-                         'measurements' : ['Trtu', 'Tref', 'Tfre', 'SOC', 'Pbattery'],
-                         'other_outputs' : ['Pnet', 'Prtu', 'Pref', 'Pfre', 'Pbattery', 'Ppv', 'Grtu'],
+                         'measurements' : ['Trtu_west', 'Trtu_east', 'Tref', 'Tfre', 'SOC', 'Pbattery'],
+                         'other_outputs' : ['Pnet', 'Prtu_west', 'Prtu_east', 'Pref', 'Pfre', 'Pbattery', 'Ppv', 'Grtu_west', 'Grtu_east'],
                          'sample_rate' : 3600,
-                         'parameters' : {'Name':      ['Trtu_0', 'Tref_0', 'Tfre_0', 'SOC_0'],
-                                         'Free':      [False,    False,    False,    False],
-                                         'Value':     [72,       34,        -5,     0.25],
-                                         'Minimum':   [212,      212,      212,      0],
-                                         'Maximum':   [-50,      -50,      -50,      1],
-                                         'Covariance':[0,        0,        0,        0],
-                                         'Unit' :     ['degF',   'degF',   'degF',   '1']},
-                          'init_vm' : {'Trtu_0' : 'Trtu',
+                         'parameters' : {'Name':      ['Trtu_west_0', 'Trtu_east_0', 'Tref_0', 'Tfre_0', 'SOC_0'],
+                                         'Free':      [False,   False,    False,    False,    False],
+                                         'Value':     [70,       70,      33,        -7,     0.25],
+                                         'Minimum':   [0,         0,      0,         -20,      0],
+                                         'Maximum':   [100,      100,      50,       100,     1],
+                                         'Covariance':[0,        0,        0,         0,      0],
+                                         'Unit' :     ['degF',   'degF',   'degF',   'degF', '1']},
+                          'init_vm' : {'Trtu_west_0' : 'Trtu_west',
+                                       'Trtu_east_0' : 'Trtu_east',
                                        'Tref_0' : 'Tref',
                                        'Tfre_0' : 'Tfre',
                                        'SOC_0'  : 'SOC'}},
@@ -33,19 +34,21 @@ config={"model_config" :{'mopath' : os.path.join('models','SolarPlus.mo'),
                      'geo' : (40.88,-124.0)},
 
 "control_config" :    {'vm'  : {
-                              'HVAC1_Norm' : ('uCool', units.unit1),
+                              'HVAC_Norm' : ('uCool', units.unit1),
                               'RefComp_Norm' : ('uRef', units.unit1),
                               'FreComp_Split_Norm' : ('uFreCool', units.unit1),
                               'uHeat' : ('uHeat', units.unit1),
                               'uBattery' : ('uBattery', units.unit1)}},
 
 "constraint_config" : {'vm'  : {
-#                              'Trtu_min':('Trtu', 'GTE', units.degC),
-#                              'Trtu_max':('Trtu', 'LTE', units.degC),
-#                              'Tref_min':('Tref', 'GTE', units.degC),
-#                              'Tref_max':('Tref', 'LTE', units.degC),
-#                              'Tfre_min':('Tfre', 'GTE', units.degC),
-#                              'Tfre_max':('Tfre', 'LTE', units.degC),
+                              'Trtu_min':('Trtu_west', 'GTE', units.degC),
+                              'Trtu_max':('Trtu_west', 'LTE', units.degC),
+                              'Trtu_min':('Trtu_east', 'GTE', units.degC),
+                              'Trtu_max':('Trtu_east', 'LTE', units.degC),
+                              'Tref_min':('Tref', 'GTE', units.degC),
+                              'Tref_max':('Tref', 'LTE', units.degC),
+                              'Tfre_min':('Tfre', 'GTE', units.degC),
+                              'Tfre_max':('Tfre', 'LTE', units.degC),
                               'SOC_min':('SOC', 'GTE', units.unit1),
                               'SOC_max':('SOC', 'LTE', units.unit1),
                               'uCool_min':('uCool', 'GTE', units.unit1),
@@ -64,37 +67,34 @@ config={"model_config" :{'mopath' : os.path.join('models','SolarPlus.mo'),
                               },
 
 "price_config" :      {'vm'  : {'pi_e':('pi_e', units.dol_kWh),
-                              'pi_d':('pi_d', units.dol_kW),
-                              'P_est':('P_est', units.kW)}},
+                                'pi_d':('pi_d', units.dol_kW),
+                                'P_est':('P_est', units.kW)}},
 
-"system_config" :     {'vm'  : {'Tref':('Tref', units.degF),
-                              'Trtu':('Trtu', units.degF),
-                              'Tfre':('Tfre', units.degF)
+"system_config" :     {'vm'  : {
+                                'Trtu_west':('Trtu_west', units.degF),
+                                'Trtu_east':('Trtu_east', units.degF),
+                                'Tref':('Tref', units.degF),
+                                'Tfre':('Tfre', units.degF)
 #                              'SOC':('SOC',units.unit1)
                               }
                        },
 "setpoints_config" :   {'vm': {'uBattery':('uBattery',units.unit1),
-                             'Pbattery':('Pbattery',units.W),
-                             'Trtu':('Trtu',units.degF),
-                             'Tref':('Tref',units.degF),
-                             'Tfre':('Tfre',units.degF)
+                               'Pbattery':('Pbattery',units.W),
+                               'Trtu_west':('Trtu_west',units.degF),
+                               'Trtu_east':('Trtu_east',units.degF),
+                               'Tref':('Tref',units.degF),
+                               'Tfre':('Tfre',units.degF)
                                }
                         },
 "data_manager_config": {
     "site": "blr",
     "source": {
         "csv_files": [
-            # "Shadow/Weather_Forecast.csv",
             "Shadow/Price_Forecast.csv",
             "Shadow/Constraints_Forecast.csv"
         ],
         "influxdb": {"config_filename":"database_client/config.yaml",
-                     "section": "database"},
-        "xbos": {
-            "namespace": "<namespace_hash>",
-            "wavemq": "localhost:4516",
-            "entity": "<data_manager_entity>"
-        }
+                     "section": "database"}
     },
     "weather": {
         "type": "influxdb",
@@ -109,7 +109,7 @@ config={"model_config" :{'mopath' : os.path.join('models','SolarPlus.mo'),
         "variables": {
             "FreComp_Split_Norm": {"filename": "Shadow/Control_InitialGuess.csv", "column": "FreComp_Split_Norm", "tz":"America/Los_Angeles", "agg": "mean", "window": "1m"},
             "RefComp_Norm": {"filename": "Shadow/Control_InitialGuess.csv", "column": "RefComp_Norm", "tz":"America/Los_Angeles", "agg": "mean", "window": "1m"},
-            "HVAC1_Norm": {"filename": "Shadow/Control_InitialGuess.csv", "column": "HVAC1_Norm", "tz":"America/Los_Angeles", "agg": "mean", "window": "1m"},
+            "HVAC_Norm": {"filename": "Shadow/Control_InitialGuess.csv", "column": "HVAC1_Norm", "tz":"America/Los_Angeles", "agg": "mean", "window": "1m"},
             "uHeat": {"filename": "Shadow/Control_InitialGuess.csv", "column": "uHeat", "tz":"America/Los_Angeles", "agg": "mean", "window": "1m"},
             "uBattery": {"filename": "Shadow/Control_InitialGuess.csv", "column": "uBattery", "tz":"America/Los_Angeles", "agg": "mean", "window": "1m"}
         }
@@ -121,12 +121,12 @@ config={"model_config" :{'mopath' : os.path.join('models','SolarPlus.mo'),
     "constraint": {
         "type": "csv",
         "variables": {
-#            "Trtu_min":  {"filename": "Shadow/Constraint_Forecast.csv", "column": "Trtu_min", "tz":"America/Los_Angeles", "agg": "mean", "window": "5m"},
-#            "Trtu_max":  {"filename": "Shadow/Constraint_Forecast.csv", "column": "Trtu_max", "tz":"America/Los_Angeles", "agg": "mean", "window": "5m"},
-#            "Tref_min":  {"filename": "Shadow/Constraint_Forecast.csv", "column": "Tref_min", "tz":"America/Los_Angeles", "agg": "mean", "window": "5m"},
-#            "Tref_max":  {"filename": "Shadow/Constraint_Forecast.csv", "column": "Tref_max", "tz":"America/Los_Angeles", "agg": "mean", "window": "5m"},
-#            "Tfre_min":  {"filename": "Shadow/Constraint_Forecast.csv", "column": "Tfre_min", "tz":"America/Los_Angeles", "agg": "mean", "window": "5m"},
-#            "Tfre_max":  {"filename": "Shadow/Constraint_Forecast.csv", "column": "Tfre_max", "tz":"America/Los_Angeles", "agg": "mean", "window": "5m"},
+            "Trtu_min":  {"filename": "Shadow/Constraints_Forecast.csv", "column": "Trtu_min", "tz":"America/Los_Angeles", "agg": "mean", "window": "5m"},
+            "Trtu_max":  {"filename": "Shadow/Constraints_Forecast.csv", "column": "Trtu_max", "tz":"America/Los_Angeles", "agg": "mean", "window": "5m"},
+            "Tref_min":  {"filename": "Shadow/Constraints_Forecast.csv", "column": "Tref_min", "tz":"America/Los_Angeles", "agg": "mean", "window": "5m"},
+            "Tref_max":  {"filename": "Shadow/Constraints_Forecast.csv", "column": "Tref_max", "tz":"America/Los_Angeles", "agg": "mean", "window": "5m"},
+            "Tfre_min":  {"filename": "Shadow/Constraints_Forecast.csv", "column": "Tfre_min", "tz":"America/Los_Angeles", "agg": "mean", "window": "5m"},
+            "Tfre_max":  {"filename": "Shadow/Constraints_Forecast.csv", "column": "Tfre_max", "tz":"America/Los_Angeles", "agg": "mean", "window": "5m"},
             "SOC_min":  {"filename": "Shadow/Constraints_Forecast.csv", "column": "SOC_min", "tz":"America/Los_Angeles", "agg": "mean", "window": "1m"},
             "SOC_max":  {"filename": "Shadow/Constraints_Forecast.csv", "column": "SOC_max", "tz":"America/Los_Angeles", "agg": "mean", "window": "1m"},
             "uCool_min":  {"filename": "Shadow/Constraints_Forecast.csv", "column": "uCool_min", "tz":"America/Los_Angeles", "agg": "mean", "window": "1m"},
@@ -154,8 +154,8 @@ config={"model_config" :{'mopath' : os.path.join('models','SolarPlus.mo'),
         "type": "influxdb",
         "variables": {
             "Tref": {"uuid": "5c69b4b6-22a0-561b-801b-72aee17c5a94", "window": "5m", "agg": "mean", "measurement": "timeseries"},
-            #"Trtu_east": {"uuid": "fd200d7e-0c46-53fc-87e4-6c8639b67b94", "window": "5m", "agg": "mean", "measurement": "timeseries"},
-            "Trtu": {"uuid": "7d48d689-5cf8-50fd-98af-22dd9868b379", "window": "5m", "agg": "mean", "measurement": "timeseries"},
+            "Trtu_west": {"uuid": "7d48d689-5cf8-50fd-98af-22dd9868b379", "window": "5m", "agg": "mean", "measurement": "timeseries"},
+            "Trtu_east": {"uuid": "fd200d7e-0c46-53fc-87e4-6c8639b67b94", "window": "5m", "agg": "mean", "measurement": "timeseries"},
             "Tfre": {"uuid": "3f493b8d-0107-569f-8968-433f46de0fec", "window": "5m", "agg": "mean", "measurement": "timeseries"}
 #            "SOC": {"uuid": "86f72439-35a3-4997-a14f-24f8a889b164", "window": "5m", "agg": "mean", "measurement": "timeseries"}
         }
