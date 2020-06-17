fd9b45ed0fb4cf6cb2e4f85c1d3c48ba492e0a28
Anand Krishnan Prakash <anandkp92@gmail.com>
2020-06-17 00:09:34 -0700

    updating forecast time to 12 hours

--- controller/control.py
+++ controller/control.py
@@ -28,7 +28,7 @@ def run():
     start_time = start.strftime("%Y-%m-%d %H:%M:00")
     start_time_utc = pd.to_datetime(start_time).tz_localize(tz_computer).tz_convert('UTC')
     #mpc_horizon = 24*3600
-    mpc_horizon = 6*3600
+    mpc_horizon = 12*3600
     # mpc_step = 3600
     print('\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%')
     print("The Solar+ Optimizer has begun its operation at {0} UTC...".format(start_time_utc))
